5533b5185764460a2a7df94d8cf665f786c7de57
aqmr-kino <32405491+aqmr-kino@users.noreply.github.com>
2021-01-18 21:57:59 +0900

    v0.1.0 -> v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='imgstorage',
-    version='0.1.0',
+    version='0.2.0',
     description='Image file auto scaling library',
     author='aqmr-kino',
     url='https://github.com/aqmr-kino/imgstorage',
